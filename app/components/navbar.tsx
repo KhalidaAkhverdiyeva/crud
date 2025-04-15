078ed344e577a157d28d6ba6371491b337bfe44e
Khalida <xalide.haqverdiyeva@gmail.com>
2025-04-15 13:54:59 +0400

    navbar removed

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -35,58 +35,7 @@ const Navbar = () => {
     <nav className="bg-white shadow-md py-4 px-[50px] flex justify-between items-center">
       <div className="flex items-center gap-[100px]">
         <img src="./trimmed.png" alt="/" className="h-[50px]" />
-        <ul className="flex gap-[50px] font-[400] text-[16px]">
-          <li>
-            <a
-              href="/home"
-              className={` hover:text-[#E3411A] transition-colors duration-400 ${
-                pathname === "/home" ? "text-[#E3411A]" : "text-black"
-              }`}
-            >
-              Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="/products"
-              className={` hover:text-[#E3411A] transition-colors duration-400 ${
-                pathname === "/products" ? "text-[#E3411A]" : "text-black"
-              }`}
-            >
-              Products
-            </a>
-          </li>
-          <li>
-            <a
-              href="/contact"
-              className={` hover:text-[#E3411A] transition-colors duration-400 ${
-                pathname === "/contact" ? "text-[#E3411A]" : "text-black"
-              }`}
-            >
-              Contact
-            </a>
-          </li>
-          <li>
-            <a
-              href="/about"
-              className={` hover:text-[#E3411A] transition-colors duration-400 ${
-                pathname === "/about" ? "text-[#E3411A]" : "text-black"
-              }`}
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="/services"
-              className={` hover:text-[#E3411A] transition-colors duration-400 ${
-                pathname === "/services" ? "text-[#E3411A]" : "text-black"
-              }`}
-            >
-              Services
-            </a>
-          </li>
-        </ul>
+     
       </div>
 
       <div className="flex items-center gap-4">
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Table from "app/components/table";
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="flex justify-center bg-[#FAF4F4]">
+      <Table />
+    </div>
+  );
+};
+
+export default Products;
